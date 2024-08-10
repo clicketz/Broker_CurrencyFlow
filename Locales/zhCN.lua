1b49f640b727aba5ac01bed8167b4262ea8d3fea
clicketz <40314230+clicketz@users.noreply.github.com>
2024-08-10 16:10:51 -0600

    Fix whitespace

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -6,7 +6,7 @@ Translator              : ttpod
 Revision                : $Rev: 36 $
 ********************************************************************* ]]
 
-local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale( "Currencyflow", "zhCN" )
+local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Currencyflow", "zhCN")
 if not L then return end
-  
+
 --@localization(locale="zhCN", format="lua_additive_table")@
